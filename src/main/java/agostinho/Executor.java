2e4e8c790b35d383eb20d5b3b5d4f65c38fa7cf0
Fernando Agostinho <agostinho.ufsc@gmail.com>
2017-07-19 14:54:51 -0300

    treinamento deletado

--- src/main/java/agostinho/Executor.java
+++
@@ -1,9 +0,0 @@
-package agostinho;
-
-import org.openqa.selenium.WebDriver;
-
-public interface Executor {
-
-	public void exec(WebDriver webdriver);
-
-}
--- src/main/java/agostinho/config/Config.java
+++
@@ -1,7 +0,0 @@
-package agostinho.config;
-
-public interface Config {
-
-	String URL_GOOGLE = "https://www.google.com.br/";
-
-}
